9b456e6207f5d99265a4ab76a37e6842e7790ac6
unknown <a.mardama@rt-iut.re>
2021-01-26 20:30:37 +0100

    correction oubliée

--- venv/Scripts/tchai/Tchai-cryptographique.py
+++ venv/Scripts/tchai/Tchai-cryptographique.py
@@ -2,6 +2,9 @@ from flask import Flask,  redirect, url_for, request, render_template
 import hashlib
 import datetime
 from operator import itemgetter
+from Crypto import Random
+from Crypto.PublicKey import RSA
+import base64
 
 app = Flask(__name__)
 
@@ -37,6 +40,24 @@ def verifIntegrite(tuple):
 	else:
 		integre = False
 	return integre
+
+# Cryptographie
+def generate_keys():
+	# RSA modulus length must be a multiple of 256 and >= 1024
+	modulus_length = 256*4 # use larger value in production
+	privatekey = RSA.generate(modulus_length, Random.new().read)
+	publickey = privatekey.publickey()
+	return privatekey, publickey
+
+def encrypt_message(a_message , privatekey):
+	encrypted_msg = privatekey.encrypt(a_message, 32)[0]
+	encoded_encrypted_msg = base64.b64encode(encrypted_msg) # base64 encoded strings are database friendly
+	return encoded_encrypted_msg
+
+def decrypt_message(encoded_encrypted_msg, publickey):
+	decoded_encrypted_msg = base64.b64decode(encoded_encrypted_msg)
+	decoded_decrypted_msg = publickey.decrypt(decoded_encrypted_msg)
+	return decoded_decrypted_msg
 
 transactions = getDatas()
 hash_i = None
---
+++ venv/Scripts/tchai/tests/tchai3_Attaque.ps1
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+$file=$args[0]
+[string] $index=$args[1]
+Add-Content $file $index
